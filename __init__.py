4a629ab5cfc3bdccc0a405ce44af87ede52ddfd0
LEONETTI Cédric <146001146+Cedric666666@users.noreply.github.com>
2024-11-21 14:50:41 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,6 +7,11 @@ import sqlite3
                                                                                                                                        
 app = Flask(__name__)   
 
+@app.route("/histogramme/")
+def afficher_histogramme():
+    return render_template("histogramme.html")
+
+
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
