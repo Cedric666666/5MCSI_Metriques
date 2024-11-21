67dd038c6ff6dc6028d64276b249b9e8cd1e36e4
LEONETTI Cédric <146001146+Cedric666666@users.noreply.github.com>
2024-11-21 15:11:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,21 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
+
+
+def fetch_commit_minutes():
+    url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
+    response = requests.get(url)
+    commits = response.json()
+    minutes = []
+
+    for commit in commits:
+        commit_date = commit["commit"]["author"]["date"]
+        date_object = datetime.strptime(commit_date, '%Y-%m-%dT%H:%M:%SZ')
+        minutes.append(date_object.minute)
+
+    return minutes
+
                                                                                                                                        
 app = Flask(__name__)   
 
